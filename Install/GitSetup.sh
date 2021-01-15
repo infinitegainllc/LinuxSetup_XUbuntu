0082e1b269b095bb3a173864f10648a4fcc9bf25
infinitegainllc <74797385+infinitegainllc@users.noreply.github.com>
2021-01-14 20:32:07 -0600

    adding git install bash

---
+++ Install/GitSetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# install git bash
+
+username=YourUserNameHere
+gitEmail=YourEmailHere
+
+echo "update package index"
+sudo apt update
+
+echo "install git"
+sudo apt install git
+
+echo "check git version"
+git --version
+
+echo "git install script done"
+
+echo "set com global values"
+echo "set username"
+git config --global user.name $username --replace-all
+echo "set email"
+git config --global user.email $gitEmail
+echo "check global settings"
+git config --list
