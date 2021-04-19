a23d171e10b22a3af140a53cae94b30ae9a0b2a3
InfiniteGain <InfiniteGainLLC@gmail.com>
2021-04-18 21:43:12 -0500

    vscode script changes

--- Install/VSCode.sh
+++ Install/VSCode.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 echo "install VScode"
-echo "update packages"
-#sudo apt update
-sudo apt install software-properties-common apt-transport-https wget
-echo "import the Microsoft GPG key using the following wget command"
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-echo "enable the Visual Studio Code repository"
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-echo "Once the apt repository is enabled , install the latest version of Visual Studio Code"
-#sudo apt update
-sudo apt install code
+
+
+#old method of installing vs code
+###echo "update packages"
+####sudo apt update
+###sudo apt install software-properties-common apt-transport-https wget
+###echo "import the Microsoft GPG key using the following wget command"
+###wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+###echo "enable the Visual Studio Code repository"
+###sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+###echo "Once the apt repository is enabled , install the latest version of Visual Studio Code"
+####sudo apt update
+###sudo apt install code
+
+sudo snap install --classic code
 
 echo "list installed vscode extensions that can be copied and ran from terminal to install somewhere else"
 code --list-extensions | xargs -L 1 echo code --install-extension
